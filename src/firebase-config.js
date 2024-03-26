4231380cc6838d8170987c3da4bf47b68abfcf99
chrisxie977 <68816452+chrisxie977@users.noreply.github.com>
2024-03-26 16:39:59 +0800

    Update firebase-config.js
    Remove API key

--- src/firebase-config.js
+++ src/firebase-config.js
@@ -5,13 +5,7 @@ import { getAuth } from 'firebase/auth';
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyAYw3nIAqaFU0B5zF9JB6b9AZZvQDStZwI",
-  authDomain: "swiftcollab.firebaseapp.com",
-  projectId: "swiftcollab",
-  storageBucket: "swiftcollab.appspot.com",
-  messagingSenderId: "411635818787",
-  appId: "1:411635818787:web:aa878f876f547034d0ac96",
-  measurementId: "G-9EPXTBEL3D"
+
 };
 
 // Initialize Firebase
